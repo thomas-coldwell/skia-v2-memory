66d90a27bbdf2beaa5d3df0fa6392e2b4a33da7c
Thomas Coldwell <31568400+thomas-coldwell@users.noreply.github.com>
2025-05-16 15:04:54 +0100

    feat: initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Canvas,
+  Image,
+  PaintStyle,
+  Skia,
+  SkImage,
+} from "@shopify/react-native-skia";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { runOnUI, useSharedValue } from "react-native-reanimated";
+
+const SIZE = 300;
 
 export default function App() {
+  const texture = useSharedValue<SkImage | null>(null);
+  const isRunning = useSharedValue(false);
+
+  const runSkia = () => {
+    runOnUI(() => {
+      "worklet";
+      const surface = Skia.Surface.MakeOffscreen(SIZE, SIZE);
+      if (surface == null) {
+        return;
+      }
+      const canvas = surface.getCanvas();
+      const colors = [
+        Skia.Color("red"),
+        Skia.Color("green"),
+        Skia.Color("blue"),
+        Skia.Color("yellow"),
+        Skia.Color("purple"),
+        Skia.Color("orange"),
+        Skia.Color("pink"),
+        Skia.Color("cyan"),
+      ];
+      const render = () => {
+        if (!isRunning.value) {
+          return;
+        }
+        const color = colors[Math.floor(Math.random() * colors.length)];
+        const paint = Skia.Paint();
+        paint.setColor(color);
+        paint.setStyle(PaintStyle.Fill);
+        canvas.drawRect(Skia.XYWHRect(0, 0, SIZE, SIZE), paint);
+        surface.flush();
+        const snapshot = surface.makeImageSnapshot({
+          x: 0,
+          y: 0,
+          width: SIZE,
+          height: SIZE,
+        });
+        const prevTexture = texture.value;
+        texture.value = snapshot;
+        prevTexture?.dispose();
+        requestAnimationFrame(render);
+      };
+      isRunning.value = true;
+      render();
+    })();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Run Skia" onPress={runSkia} />
+      <Button title="Stop Skia" onPress={() => (isRunning.value = false)} />
+      <Canvas style={styles.canvas}>
+        <Image image={texture} x={0} y={0} width={SIZE} height={SIZE} />
+      </Canvas>
     </View>
   );
 }
@@ -13,8 +74,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  canvas: {
+    width: SIZE,
+    height: SIZE,
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
